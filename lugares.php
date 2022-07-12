6e5ebbcc802f66cf2f2871f3f6dd0d89c47b4f41
juanunsa <74318601+juanunsa@users.noreply.github.com>
2022-07-12 12:55:12 -0300

    Update lugares.php
    agregar columnas

--- lugares.php
+++ lugares.php
@@ -4,7 +4,7 @@ $query = mysqli_query($con, "SELECT nombre,ST_X (coordenadas) as latitud,ST_Y(co
  longitud,ST_Distance_Sphere(coordenadas,Point(-26.065144,-65.972699)) /1000 as terminal FROM lugar ORDER BY nombre ASC;");
 //echo mysqli_num_rows($query);
 while($row = mysqli_fetch_array($query)) { 
-    $json[]=array('nombre'=> $row['nombre'],'latitud'=>$row['latitud'],'longitud'=>$row['longitud'],'terminal'=>round($row['terminal'],1),'geologia'=>$row['geologia']) ; 
+    $json[]=array('nombre'=> $row['nombre'],'latitud'=>$row['latitud'],'longitud'=>$row['longitud'],'terminal'=>round($row['terminal'],1),'geologia'=>$row['geologia'],'patrimonio'=>$row['patrimonio'],'artesanias'=>$row['artesanias']) ; 
 }
 
 $json_string = json_encode($json); 
