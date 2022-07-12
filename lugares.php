3554d67ea4c183fd7d6a52e3dbea9507298f6583
juanunsa <74318601+juanunsa@users.noreply.github.com>
2022-07-12 12:47:38 -0300

    Update lugares.php

--- lugares.php
+++ lugares.php
@@ -1,7 +1,7 @@
 <?php
 include 'conexion.php';
 $query = mysqli_query($con, "SELECT nombre,ST_X (coordenadas) as latitud,ST_Y(coordenadas) as
- longitud,ST_Distance_Sphere(coordenadas,Point(-26.065144,-65.972699)) /1000 as terminal FROM lugar ORDER BY nombre DESC;");
+ longitud,ST_Distance_Sphere(coordenadas,Point(-26.065144,-65.972699)) /1000 as terminal FROM lugar ORDER BY nombre ASC;");
 //echo mysqli_num_rows($query);
 while($row = mysqli_fetch_array($query)) { 
     $json[]=array('nombre'=> $row['nombre'],'latitud'=>$row['latitud'],'longitud'=>$row['longitud'],'terminal'=>round($row['terminal'],1)) ; 
